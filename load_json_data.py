d1332bd88b9de6fd2dc9e01303f84cc70eb7df30
villi02 <villiarnar1@gmail.com>
2022-03-18 20:53:50 +0100

    fixed file problem

--- load_json_data.py
+++ load_json_data.py
@@ -1,5 +1,6 @@
 import json
 import fetch_from_planetary
+import pystac
 
 def load_json_data(filename="data/benthic.json"):
     with open(filename, "r") as f:
@@ -8,10 +9,12 @@ def load_json_data(filename="data/benthic.json"):
 
 if __name__ == "__main__":
     coral_reef = load_json_data()[12341]
-    #for i in range(300):
-     #   print(fetch_from_planetary.search({"coordinates": coral_reef['geometry']['coordinates']}).get_all_items_as_dict()["features"][i]["assets"]["rendered_preview"])
-    print(fetch_from_planetary.search({"coordinates": coral_reef['geometry']['coordinates']}).get_all_items_as_dict()["features"][0])
-    #for coral_reef in load_json_data():
-    #   print(coral_reef)
-
-    #print(len(load_json_data()))
+    aset = set()
+    for coral_reef in load_json_data()[::1000]:
+        fetched = fetch_from_planetary.search({"coordinates": coral_reef['geometry']['coordinates']}).get_all_items_as_dict()['features']
+        for fetch in fetched:
+            if(fetch['properties']['eo:cloud_cover'] > 5):
+                continue
+            aset.add(fetch['assets']['rendered_preview']['href'])
+        #print(fetch_from_planetary.search({"coordinates": coral_reef['geometry']['coordinates']}).get_all_items_as_dict())
+    print(aset, "\n", len(aset))
