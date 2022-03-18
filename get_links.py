31d16584c8d7f016495ab89b495df4203fae61d9
villi02 <villiarnar1@gmail.com>
2022-03-18 21:34:53 +0100

    fixed get_link and made a new download function

--- get_links.py
+++ get_links.py
@@ -8,11 +8,21 @@ def get_links():
     for reef in data[::20000]:
         link_data = search({"coordinates": reef['geometry']['coordinates']}).get_all_items_as_dict()["features"]
         for link_dict in link_data:
+            if (link_dict['properties']['eo:cloud_cover'] > 5):
+                continue
             link = link_dict["assets"]["rendered_preview"]["href"]
-            print(link)
             links.add(link)
     return links
 
+import requests
+def download_pictures(links):
+    for pic in links:
+        img_data = requests.get(pic["assets"]["rendered_preview"]["href"]).content
+        with open('image_name.jpg', 'wb') as handler:
+            handler.write(img_data)
 
 if __name__ == "__main__":
-    get_links()
+    links = get_links()
+    print("Links have been secured")
+    download_pictures(links)
+    print("Pictures should have been downloaded")
