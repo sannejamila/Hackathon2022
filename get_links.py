3d8f34d96745cdf12589712dc5d7bb8a600acb3a
Espen Boman Fosseide <espenbfo@stud.ntnu.no>
2022-03-19 07:31:22 +0100

    Update link_getter, random link sampler, and pushed scraped links

--- get_links.py
+++ get_links.py
@@ -1,28 +1,57 @@
 from load_json_data import load_json_data
 from fetch_from_planetary import search
+import requests
+import json
+import os
 
 def get_links():
     data = load_json_data()
 
     links = set()
-    for reef in data[::20000]:
+    length = len(data[::20])
+    for i, reef in enumerate(data[::20]):
+        print(i, "of", length)
         link_data = search({"coordinates": reef['geometry']['coordinates']}).get_all_items_as_dict()["features"]
         for link_dict in link_data:
-            if (link_dict['properties']['eo:cloud_cover'] > 5):
+            if (link_dict['properties']['eo:cloud_cover'] > 30):
                 continue
             link = link_dict["assets"]["rendered_preview"]["href"]
             links.add(link)
     return links
 
-import requests
-def download_pictures(links):
-    for pic in links:
-        img_data = requests.get(pic["assets"]["rendered_preview"]["href"]).content
-        with open('image_name.jpg', 'wb') as handler:
-            handler.write(img_data)
+def save_links(links, filename):
+    print("Number of links", len(links))
+    with open(filename, "w") as f:
+        json.dump(list(links), f)
+    print("Successfully saved links to", filename)
+
+def load_links(filename):
+
+    with open(filename, "r") as f:
+        links = json.load(f)
+    print(f"Successfully loaded {len((links))} links")
+    return links
+
+def download_pictures(links, data_folder):
+    total_number = len(links)
+    for i, pic in enumerate(links):
+        try:
+            print(f"downloading nr. {i} of {total_number}")
+            img_data = requests.get(pic).content
+            with open(os.path.join(data_folder, f'{i}_image_name.jpg'), 'wb') as handler:
+                handler.write(img_data)
+        except Exception as e:
+            print("error in download:", e)
+
+    print("download complete")
 
 if __name__ == "__main__":
     links = get_links()
-    print("Links have been secured")
-    download_pictures(links)
-    print("Pictures should have been downloaded")
+    save_links(links, "data/links_cloudy.json")
+    links = load_links("data/links_cloudy.json")
+    download_pictures(links, "data/corals_cloudy")
+    #
+    # links = get_links()
+    # print("Links have been secured")
+    # download_pictures(links)
+    # print("Pictures should have been downloaded")
--- sample_random_images.py
+++ sample_random_images.py
@@ -1,13 +1,15 @@
 from global_land_mask import globe
 from random import uniform
 from fetch_from_planetary import search
+from get_links import load_links, download_pictures, save_links
+
 
 def sample_random_images(top_left_corner, bottom_right_corner, num, coral_links, batch_size=50):
     l, t, r, b = (*top_left_corner, *bottom_right_corner)
 
     num_coords = 0
     links = set()
-    while num_coords < num:
+    while len(links) < num:
         batch = [[]]
         for i in range(batch_size):
 
@@ -16,21 +18,17 @@ def sample_random_images(top_left_corner, bottom_right_corner, num, coral_links,
 
             if not globe.is_land(x, y):
                 batch[0].append([y, x])
-        print("start")
-        print("Coords", batch)
         link_data = search({"coordinates": batch}).get_all_items_as_dict()["features"]
-        print("end")
-        print(link_data)
         for link_dict in link_data:
             link = link_dict["assets"]["rendered_preview"]["href"]
-            print(link)
             if link not in coral_links:
-                print("include")
                 links.add(link)
-                num_coords += 1
+        print(len(links), "of", num)
 
     return links
 
 if __name__ == "__main__":
-    links = sample_random_images((-12.809395673491847, 139.2690916581671), (-17.706255347155274, 141.75589144731404), 8, {"https::/empty.link"})
-    print(links)
+    links = sample_random_images((-10.822170746118468, 137.92321102215595), (-17.706255347155274, 141.75589144731404), 3000, {"https::/empty.link"})
+    save_links(links, "data/ocean_links.json")
+    links = load_links("data/ocean_links.json")
+    download_pictures(links, "data/ocean")
