1eabea365ad7f4f9ced87bcd08eaa145c683c348
espen <espenbfo@stud.ntnu.no>
2022-03-19 20:24:41 +0100

    Add flask server and masking algorithm

---
+++ flask_server.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify, send_file
+from flask_restful import Resource, Api
+from manual_detection import detect_corals_in_image, red_ring_image
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+api = Api(app)
+
+todos = {"id": "hello"}
+
+global_size = 0
+@app.route("/img", methods=["POST"])
+def process_image():
+    print(request.files["image"])
+    file = request.files["image"]
+    file.save("image_file.png")
+    # Read the image via file.stream
+    img = cv2.imread("image_file.png")
+    image, size = red_ring_image(img)
+    global_size = size
+
+    cv2.imwrite("edited_file.png", image)
+
+
+    return send_file("edited_file.png", mimetype="image/gif")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- sample_random_images.py
+++ sample_random_images.py
@@ -1,3 +1,5 @@
+import os
+
 from global_land_mask import globe
 from random import uniform
 from fetch_from_planetary import search
@@ -9,6 +11,7 @@ def sample_random_images(top_left_corner, bottom_right_corner, num, coral_links,
 
     num_coords = 0
     links = set()
+    print("Halla")
     while len(links) < num:
         batch = [[]]
         for i in range(batch_size):
@@ -18,7 +21,9 @@ def sample_random_images(top_left_corner, bottom_right_corner, num, coral_links,
 
             if not globe.is_land(x, y):
                 batch[0].append([y, x])
+        print("search")
         link_data = search({"coordinates": batch}).get_all_items_as_dict()["features"]
+        print("searchend")
         for link_dict in link_data:
             link = link_dict["assets"]["rendered_preview"]["href"]
             if link not in coral_links:
@@ -28,7 +33,10 @@ def sample_random_images(top_left_corner, bottom_right_corner, num, coral_links,
     return links
 
 if __name__ == "__main__":
-    links = sample_random_images((-10.822170746118468, 137.92321102215595), (-17.706255347155274, 141.75589144731404), 3000, {"https::/empty.link"})
-    save_links(links, "data/ocean_links.json")
-    links = load_links("data/ocean_links.json")
-    download_pictures(links, "data/ocean")
+    links = sample_random_images((45.56794039153187, 164.79086620168565), (27.918165561879146, -165.96225727163014), 2300, {"https::/empty.link"})
+    save_links(links, "data/ocean_links3.json")
+    links = load_links("data/ocean_links3.json")
+    os.makedirs("data/ocean3", exist_ok=True)
+    download_pictures(links, "data/ocean3")
+    #-5.289254923198517, 114.32078803511483
+    #-3.0940141936830634, 109
